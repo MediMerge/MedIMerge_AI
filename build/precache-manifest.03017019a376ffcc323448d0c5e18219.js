547ec7b119ab0428b58c7e0e9e92eb54e277c38d
Pratyush Mittal <109898014+prat0404@users.noreply.github.com>
2024-06-19 12:10:03 +0530

    AI_update
    MediMerge_AI

---
+++ build/precache-manifest.03017019a376ffcc323448d0c5e18219.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28647703969ca363464d064c573c9c1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86742fc6dc6b3b355488",
+    "url": "/static/css/main.b429c2c8.chunk.css"
+  },
+  {
+    "revision": "17244c5adf9d057102a2",
+    "url": "/static/js/2.2908c49b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.2908c49b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86742fc6dc6b3b355488",
+    "url": "/static/js/main.58dcb291.chunk.js"
+  },
+  {
+    "revision": "f71aa9cea26c09e93276",
+    "url": "/static/js/runtime-main.bc38b3b3.js"
+  },
+  {
+    "revision": "7c583c79e665fe726210608b348137e1",
+    "url": "/static/media/logo.7c583c79.svg"
+  }
+]);
